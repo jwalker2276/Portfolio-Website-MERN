4c4456ffe6d5ccae34b16814a37dd5994bb90853
Jordan <jordanwalker@Jordans-MacBook-Pro.local>
2019-01-27 16:52:17 -0600

    Made style changes for phone layout.

--- server/.next/static/runtime/webpack.js
+++ server/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "781682a20f05e291fad5";
+/******/ 	var hotCurrentHash = "1413875f9b7cad18aac9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
