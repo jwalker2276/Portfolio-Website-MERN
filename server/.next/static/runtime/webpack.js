ce2427d461ad30b9ca4f46aeeff9df3f390f72d9
Jordan <jordanwalker@Jordans-MacBook-Pro.local>
2019-02-19 20:27:01 -0600

    Updated about section styling.

--- server/.next/static/runtime/webpack.js
+++ server/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fccff37a69f6301aa826";
+/******/ 	var hotCurrentHash = "5c8134a6e411a6118556";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
