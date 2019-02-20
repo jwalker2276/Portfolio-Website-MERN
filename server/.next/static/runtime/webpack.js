0bbd041813ae54301408a97f75926e6ac9b4936c
Jordan <jordanwalker@Jordans-MacBook-Pro.local>
2019-02-19 20:47:56 -0600

    Compressed landing page image.

--- server/.next/static/runtime/webpack.js
+++ server/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5c8134a6e411a6118556";
+/******/ 	var hotCurrentHash = "88bc3d18c42ab2107668";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
