dd0395bc3fc751349fb1a4f1be7406b90f0bf33a
Jordan <jordanwalker@Jordans-MacBook-Pro.local>
2019-02-23 21:48:04 -0600

    Updated meta component and changed package.json for deployment.

--- server/components/common/Meta.js
+++ server/components/common/Meta.js
@@ -6,6 +6,12 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <title>JWalker Creations</title>
+    <meta name="description" content="My portfolio website." />
+    <meta
+      name="keywords"
+      content="jordan walker, jordan, website, web development, responsive, developer, portfolio"
+    />
+    <meta name="author" content="Jordan Walker" />
     <link
       href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Raleway:300,400,500,600,700"
       rel="stylesheet"
